64e038666ff2fb3593a86a98a10b86dba97c465c
Faiz Surani <faiz.surani@gmail.com>
2025-06-20 22:21:24 -0700

    Remove old deps and add new openapi config

--- frontend/openapi-ts.config.ts
+++ frontend/openapi-ts.config.ts
@@ -1,30 +1,38 @@
 import { defineConfig } from "@hey-api/openapi-ts";
 
 const args = process.argv.slice(2);
-const host = args[0] || "poirot:8101";
+const host = args[0] || "localhost:8101";
 
 export default defineConfig({
-  client: "@hey-api/client-fetch",
   input: host.includes("http")
     ? `${host}/openapi.json`
     : `http://${host}/openapi.json`,
   output: {
+    indexFile: true,
     path: "src/client",
     format: "biome",
   },
   plugins: [
-    "@tanstack/react-query",
+    {
+      name: "@hey-api/client-axios",
+      baseUrl: false,
+    },
+    {
+      name: "@tanstack/react-query",
+    },
     {
       name: "@hey-api/transformers",
       dates: true,
     },
     {
-      name: "@hey-api/services",
+      name: "@hey-api/sdk",
       asClass: true,
+      transformer: true,
     },
     {
-      name: "@hey-api/types",
+      name: "@hey-api/typescript",
       enums: "javascript",
+      readOnlyWriteOnlyBehavior: "off",
     },
   ],
 });
