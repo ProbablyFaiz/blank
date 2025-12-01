3b21084b38f8ddd6abc107148fe82cbc76819bec
Faiz Surani <faiz.surani@gmail.com>
2025-12-01 10:01:53 -0800

    Add db migration CLI

--- backend/blanket/cli/main.py
+++ backend/blanket/cli/main.py
@@ -1,9 +1,13 @@
+import blanket.db.commands as db_commands
 import blanket.io.click as click
 
 
 @click.group()
 def cli():
     pass
+
+
+cli.add_command(db_commands.db)
 
 
 if __name__ == "__main__":
